2b4247e303722869bd41aa60fd9e81d4d76f9b10
RonanSteinert <116797065+RonanSteinert@users.noreply.github.com>
2023-03-07 21:47:04 +0100

    problem super class

--- src/main/java/com/studiomedico/Studio/Medico/entities/Persona.java
+++ src/main/java/com/studiomedico/Studio/Medico/entities/Persona.java
@@ -9,27 +9,17 @@ public class Persona {
     private String surname;
     private String email;
     private String address;
-    private String city;
     private String telephone;
-    private LocalDate birthDate;
-    private String description;
-    private String birthCity;
-    private StatusGender gender;
 
     public Persona() {
     }
 
-    public Persona(String name, String surname, String email, String address, String city, String telephone, LocalDate birthDate, String description, String birthCity, StatusGender gender) {
+    public Persona(String name, String surname, String email, String address, String telephone) {
         this.name = name;
         this.surname = surname;
         this.email = email;
         this.address = address;
-        this.city = city;
         this.telephone = telephone;
-        this.birthDate = birthDate;
-        this.description = description;
-        this.birthCity = birthCity;
-        this.gender = gender;
     }
 
     public String getName() {
@@ -64,51 +54,11 @@ public class Persona {
         this.address = address;
     }
 
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
     public String getTelephone() {
         return telephone;
     }
 
     public void setTelephone(String telephone) {
         this.telephone = telephone;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getBirthCity() {
-        return birthCity;
-    }
-
-    public void setBirthCity(String birthCity) {
-        this.birthCity = birthCity;
-    }
-
-    public StatusGender getGender() {
-        return gender;
-    }
-
-    public void setGender(StatusGender gender) {
-        this.gender = gender;
     }
 }
--- src/main/java/com/studiomedico/Studio/Medico/entities/ProvaUser.java
+++ src/main/java/com/studiomedico/Studio/Medico/entities/ProvaUser.java
@@ -1,13 +1,22 @@
 package com.studiomedico.Studio.Medico.entities;
 
+import com.studiomedico.Studio.Medico.statusEnum.StatusGender;
+import jakarta.persistence.*;
+import org.springframework.web.bind.annotation.Mapping;
+
+import java.time.LocalDate;
+
+@Entity
+@Table
 public class ProvaUser extends Persona{
-    @Override
-    public String getName() {
-        return super.getName();
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    // di solito così creiamo una super classe ma come facciamo a mettere sti campi sulla tabella?
+    // avete qualche idea? qualche annotazione?
+    public ProvaUser(String name, String surname, String email, String address, String telephone, long id) {
+        super(name, surname, email, address, telephone);
+        this.id = id;
     }
 
-    @Override
-    public void setName(String name) {
-        super.setName(name);
-    }
 }
